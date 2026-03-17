595a0695b63d493b6e507a0702dba69a4cd6a83a
ivars-M <ivars.muizarajs@gmail.com>
2026-03-17 10:12:28 +0200

    Fix CORS origin and DB user spelling

--- src/axios.js
+++ src/axios.js
@@ -20,16 +20,18 @@ import axios from "axios";
 
 const instance = axios.create({
   // Atgriežamies pie vienkāršības, kas strādāja
-  baseURL: window.location.hostname === "localhost" 
-    ? "http://localhost:4444" 
-    : "https://my-blog-backend-qniv.onrender.com",
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:4444"
+      : "https://my-blog-backend-qniv.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
   const token = window.localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = token;
+    // config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
